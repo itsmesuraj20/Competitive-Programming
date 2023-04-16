dc75893775fc0c037cce401d9088d2037bc161a4
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-04-17 01:48:47 +0530

    First question solve from A2OJ-"https://earthshakira.github.io/a2oj-clientside/server/Ladder11.html"

--- YoungPhysicist.cpp
+++ YoungPhysicist.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
-    return 0;
+   int n,i,j,f=0,sum=0;
+   cin>>n;
+   int a[n][3];
+   for ( i = 0; i < n; i++)
+   {
+    for ( j = 0; j < 3; j++)
+    {
+        cin>>a[i][j];          
+    }
+   }
+   for ( j = 0; j < 3; j++)
+   {
+    for ( i = 0; i < n; i++)
+    {
+        sum=sum+a[i][j];
+    }
+    if(sum==0)
+    f=1;
+    else{
+        f=0;
+        break;
+    }
+    
+   }
+   if(f==1)
+   {
+    cout<<"YES";
+   }
+   else{
+    cout<<"NO";
+   }
+   
+   return 0;
 }
